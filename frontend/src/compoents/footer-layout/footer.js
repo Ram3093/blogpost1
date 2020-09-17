578ef2c2a76f6f80484eaf9b28398c64e03ede1c
Ram kumar <rk16013093@gmail.com>
2020-09-18 00:48:56 +0530

    footer section

---
+++ frontend/src/compoents/footer-layout/footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const  footer =()=>{
+        return (
+            <div className='fluid-container bg-light footer'>
+                <div className='container'>
+                     <div>
+                     <p>facbook</p>
+                     <p>facbook</p>
+                     <p>facbook</p>
+                     </div>
+                </div>
+            </div>
+        )
+    
+}
+
+export default footer
