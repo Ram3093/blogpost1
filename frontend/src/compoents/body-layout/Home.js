90e858ed378c57b1c1cccd54233a08c4e38d493a
Ram kumar <rk16013093@gmail.com>
2020-09-18 00:43:49 +0530

    home  section

---
+++ frontend/src/compoents/body-layout/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+    render() {
+        return (
+            <div className='container home'>
+               <div>
+                   <h2>Blog title1</h2>
+                   <h5>Blog snippet</h5>
+                   <p>blog body and lots of its information, the more you make more you shine .</p>
+               </div>
+            </div>
+        )
+    }
+}
+
+export default Home
