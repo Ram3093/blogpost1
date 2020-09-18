23b5f5983b99ca8afe7ee2694c624f35ccfc5ee3
Ram kumar <rk16013093@gmail.com>
2020-09-18 20:39:34 +0530

    add database config in app.js backend

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,27 @@
+require('dotenv').config()
 const express=require('express');
 const bodyParser = require('body-parser');
-var   cors = require('cors')
+var   cors = require('cors');
+const mongoose =require("mongoose");
 const routes=require('./Routes/auth')
 
 const port =4000;
 
 // app instance from express
 const app=express();
+
+
+// database connection
+const dbURI = process.env.MONGO_DB_URI;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(port,(req,res)=>{ console.log('server is running')}))
+  .catch((err) => console.log(err));
+
+
+
 // for get data from other server 
 app.use(cors())
 //for pass data in form and receive data in request object 
 app.use(bodyParser.json());
 
 app.use('/blogs',routes)
-
-//listening on a port 
-app.listen(port,(req,res)=>{
-    console.log("load backend server successfully",port);
-})
