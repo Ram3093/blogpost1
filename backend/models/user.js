9814d2e109567de27691aba6f3a602e019e88f10
Ram kumar <rk16013093@gmail.com>
2020-09-18 20:41:26 +0530

    updated user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,8 @@
 const mongoose=require('mongoose');
+const { isEmail }=require('validator')
+const bcrypt=require('bcrypt')
 
-const userSchmea=new mongoose.Schema({
+const userSchema=new mongoose.Schema({
 
     firstname:{
         type:String,
@@ -16,16 +18,43 @@ const userSchmea=new mongoose.Schema({
         type:String,
         required:[true,'email is required'],
         maxlength:32,
-        unique:true
+        unique:true,
+        validate:[isEmail,'email is not valid']
     },
     password:{
         type:String,
         required:[true,'password is required'],
         minlength:[6,'minimum password is 6 character']
+    },
+    username:{
+        type:String
     }
 
 })
 
-const blogUser=mongoose.model('blogUser',userSchmea)
+
+userSchema.pre('save',async function(next){
+    const salt=await bcrypt.genSalt();
+    this.password=await bcrypt.hash(this.password,salt);
+    this.username=this.firstname.slice(0,1)+this.lastname.slice(0,1);
+    next();
+});
+
+
+//static method to login user
+userSchema.statics.login=async function(email,password){
+    const user=await this.findOne( { email } );
+    if(user){
+        const auth=await bcrypt.compare(password,user.password);
+        if(auth){
+            return user;
+        }
+        throw Error('incorrect password');
+    }
+    throw Error('incorrect email');
+}
+
+
+const blogUser=mongoose.model('blogUser',userSchema)
 
 module.exports=blogUser
