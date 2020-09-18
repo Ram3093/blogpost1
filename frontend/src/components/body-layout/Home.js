115e7312beb72fc74c30f17abbe5f9c539584cd0
Ram kumar <rk16013093@gmail.com>
2020-09-18 22:26:12 +0530

    get data from db in home

--- frontend/src/components/body-layout/Home.js
+++ frontend/src/components/body-layout/Home.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
+import { api } from '../../backend'
+import axios from 'axios'
+import moment from 'moment'
 
 export class Home extends Component {
+    state={
+        blogs:[ ]
+    }
+    componentDidMount(){
+       axios.get(`${api}`)
+        .then(res=>{
+            this.setState({ blogs:res.data.slice(0,10)})
+        })
+    }
     render() {
+        const { blogs }=this.state;
+        const allblogs=blogs.length?(
+            blogs.map(el=>{
+                return (
+                    <div  key={el._id}>
+                       <h2>{el.blogtitle}</h2>
+                       <h5>{ el.blogsnippet }</h5>
+                       <p  style={{marginBottom:"0"}}>{ el.blogbody }</p>
+                        <p>{ moment(el.createdAt).calendar() }</p>
+                    </div>
+                )
+            })
+           
+        ):(<div><p>nothing to show </p></div>)
         return (
             <div className='container home'>
                <div>
-                   <h2>Blog title1</h2>
-                   <h5>Blog snippet</h5>
-                   <p>blog body and lots of its information, the more you make more you shine .</p>
+                 { allblogs }
                </div>
             </div>
         )
