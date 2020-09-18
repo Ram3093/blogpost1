e15cb353ad0a9a6f412de23ac7de2e0e20b2bed6
Ram kumar <rk16013093@gmail.com>
2020-09-18 22:30:40 +0530

    updated signinlinks

--- frontend/src/components/nav-layout/SignInlink.js
+++ frontend/src/components/nav-layout/SignInlink.js
@@ -1,18 +1,26 @@
-import React  from 'react'
+import React ,{ Component } from 'react'
 import { Link } from 'react-router-dom'
 
- const SignInLink=()=>{
-        return (
-          
-                <ul className="navbar-nav justify-content-center">
-                     <li className='nav-item' ><Link className='nav-link color-black' to="/blogs">Blogs</Link></li>
-                     <li className='nav-item' ><Link className='nav-link color-black' to="/blogs/about">About</Link></li>
-                     <li className='nav-item' ><Link className='nav-link color-black' to="/blogs/create">New Blog</Link></li>
-                     <li className='nav-item' ><Link className='nav-link color-black' to="/blogs/logout">Logout</Link></li>
-               </ul>
-          
-        )
-    
+class SignInLink extends Component {
+      state={
+
+      }
+        handleLogout=(e)=>{
+                
+        }
+
+        render() { 
+                return ( 
+                        <ul className="navbar-nav justify-content-center">
+                        <li className='nav-item' ><Link className='nav-link color-black' to="/blogs">Blogs</Link></li>
+                        <li className='nav-item' ><Link className='nav-link color-black' to="/blogs/about">About</Link></li>
+                        <li className='nav-item' ><Link className='nav-link color-black' to="/blogs/create">New Blog</Link></li>
+                        <li className='nav-item' onClick={ this.handleLogout } ><Link className='nav-link color-black' to="/blogs/logout">Logout</Link></li>
+                  </ul>
+             
+                 );
+        }
 }
+ 
 
 export default SignInLink
